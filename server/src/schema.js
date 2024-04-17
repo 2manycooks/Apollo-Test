8ebc5f6e5dac60d485e2ee7e14d54f5b5b27dbb2
2manycooks <73556731+2manycooks@users.noreply.github.com>
2024-04-17 02:47:51 -0700

    test commit

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,43 +1,29 @@
 const gql = require("graphql-tag")
 
 const typeDefs = gql`
+
     type Query {
-    "Query to get tracks array for the homepage grid"
+
     tracksForHome: [Track!]!
-    
+
     }
 
+    type Track {
 
+        id: ID!
+        title: String!
+        author: Author!
+        thumbnail: String
+        length: Int
+        modulesCount: Int
 
+    }
 
-
-"A track is a group of Modules that teaches about a specific topic"
-type Track {
-id: ID!
-"track title"
-title: String!
-"track's main author"
-author: Author!
-"main illustration to display in track card or track page detail"
-thumbnail: String
-"the track's approximate length to complete, in minutes"
-length: Int
-"the number of modules this track contains"
-modules:Count
-}
-
-"Author of a complete Track"
-type Author {
-    id: ID!
-    "Author's first and last name"
-    name: String!
-    "Author's profile URL"
-    photo: String
-
-
-}
-
+    type Author {
+        id: ID!
+        name: String!
+        photo: String
+    }
 
 `
-
 module.exports = typeDefs
