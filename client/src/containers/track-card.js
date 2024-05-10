ba9584d26b6ac46b7bc78d2a8439dc3367d1b69c
2manycooks <73556731+2manycooks@users.noreply.github.com>
2024-05-10 15:42:50 -0700

    mission successful! todo: fix styling for links on home page

--- client/src/containers/track-card.js
+++ client/src/containers/track-card.js
@@ -3,6 +3,22 @@ import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
 import { Link } from "react-router-dom"
+import { gql, useMutation } from "@apollo/client"
+
+
+const INCREMENT_TRACK_VIEWS = gql`
+  mutation IncrementTrackViews($incrementTrackViewsId: ID!) {
+    incrementTrackViews(id: $incrementTrackViewsId) {
+      code
+      message
+      success
+      track {
+        id
+        numberOfViews
+      }
+    }
+  }
+`
 
 /**
  * Track Card component renders basic info in a card format
@@ -11,8 +27,15 @@ import { Link } from "react-router-dom"
 const TrackCard = ({ track }) => {
   const { title, thumbnail, author, length, modulesCount, id } = track;
 
+  const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
+    variables: { incrementTrackViewsId: id },
+    onCompleted: (data) => {
+      console.log(data)
+    }
+  })
+
   return (
-    <Link to={`/track/${id}`}>
+    <Link to={`/track/${id}`} onClick={incrementTrackViews}>
     <CardContainer >
       <CardContent>
         <CardImageContainer>
